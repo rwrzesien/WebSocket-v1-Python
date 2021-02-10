608e56051821231c66962e461ed13126766a6276
waslar <is>
2021-02-10 01:07:15 +0100

    Add `setup.py`

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from setuptools import setup
+
+
+setup(
+    name='WebSocketV1Python',
+    version='0.1',
+    url='https://github.com/rwrzesien/Websocket-v1-Python',
+    maintainer='',
+    maintainer_email='',
+    packages=[
+        'cf_websocket',
+    ],
+    package_data={},
+    include_package_data=True,
+    python_requires='>=3.6',
+)
+
